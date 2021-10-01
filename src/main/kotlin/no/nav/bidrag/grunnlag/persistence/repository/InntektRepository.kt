9cdcd2cc971b7a6ba4f6ade1d02d8a813ff639b5
rinnan17 <magnus.rinnan@nav.no>
2021-10-01 13:51:15 +0200

    Ny sql sett inntekt som innaktiv

--- src/main/kotlin/no/nav/bidrag/grunnlag/persistence/repository/InntektRepository.kt
+++ src/main/kotlin/no/nav/bidrag/grunnlag/persistence/repository/InntektRepository.kt
@@ -2,6 +2,7 @@ package no.nav.bidrag.grunnlag.persistence.repository
 
 import no.nav.bidrag.grunnlag.persistence.entity.Inntekt
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 
 interface InntektRepository : JpaRepository<Inntekt, Int?> {
@@ -10,5 +11,11 @@ interface InntektRepository : JpaRepository<Inntekt, Int?> {
     "select int from Inntekt int where int.grunnlagspakkeId = :grunnlagspakkeId and int.aktiv = true"
   )
   fun hentInntekter(grunnlagspakkeId: Int): List<Inntekt>
+
+  @Query(
+    "update Inntekt int set int.aktiv = false, int.brukTil = CURRENT_TIMESTAMP where int.inntektId = :inntektId"
+  )
+  @Modifying
+  fun settInntektSomInnaktiv(inntektId: Int)
 
 }
