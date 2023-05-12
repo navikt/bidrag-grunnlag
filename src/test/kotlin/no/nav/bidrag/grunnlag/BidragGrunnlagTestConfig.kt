d772ab05c3281ba8620cf0db01a6dd15ef433d92
Bent J. Lorentzen <bent.lorentzen@nav.no>
2023-05-12 15:53:56 +0200

    Spring boot 3

--- src/test/kotlin/no/nav/bidrag/grunnlag/BidragGrunnlagTestConfig.kt
+++ src/test/kotlin/no/nav/bidrag/grunnlag/BidragGrunnlagTestConfig.kt
@@ -1,5 +1,6 @@
 package no.nav.bidrag.grunnlag
 
+import com.nimbusds.jose.JOSEObjectType
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
@@ -7,6 +8,7 @@ import no.nav.bidrag.commons.web.test.HttpHeaderTestRestTemplate
 import no.nav.bidrag.grunnlag.BidragGrunnlagLocal.Companion.LOCAL_PROFILE
 import no.nav.bidrag.grunnlag.BidragGrunnlagTest.Companion.TEST_PROFILE
 import no.nav.security.mock.oauth2.MockOAuth2Server
+import no.nav.security.mock.oauth2.token.DefaultOAuth2TokenCallback
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.context.annotation.Bean
@@ -23,17 +25,30 @@ import org.springframework.http.HttpHeaders
 class BidragGrunnlagTestConfig {
 
     @Autowired
-    private var mockOAuth2Server: MockOAuth2Server? = null
+    private lateinit var mockOAuth2Server: MockOAuth2Server
 
     @Bean
-    fun securedTestRestTemplate(testRestTemplate: TestRestTemplate?): HttpHeaderTestRestTemplate? {
+    fun securedTestRestTemplate(testRestTemplate: TestRestTemplate): HttpHeaderTestRestTemplate? {
         val httpHeaderTestRestTemplate = HttpHeaderTestRestTemplate(testRestTemplate)
         httpHeaderTestRestTemplate.add(HttpHeaders.AUTHORIZATION) { generateTestToken() }
         return httpHeaderTestRestTemplate
     }
 
-    private fun generateTestToken(): String {
-        val token = mockOAuth2Server?.issueToken(ISSUER, "aud-localhost", "aud-localhost")
-        return "Bearer " + token?.serialize()
+    fun generateTestToken(): String {
+        val iss = mockOAuth2Server.issuerUrl("aad")
+        val newIssuer = iss.newBuilder().host("localhost").build()
+        val token = mockOAuth2Server.issueToken(
+            "aad",
+            "aud-localhost",
+            DefaultOAuth2TokenCallback(
+                "aad",
+                "aud-localhost",
+                JOSEObjectType.JWT.type,
+                listOf("aud-localhost"),
+                mapOf("iss" to newIssuer.toString()),
+                3600
+            )
+        )
+        return "Bearer " + token.serialize()
     }
 }
