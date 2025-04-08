07b424d20d18c9ee7c328d4d9fcca25f6f949d0c
Magnus Rinnan <magnus.rinnan@nav.no>
2025-04-08 16:04:45 +0200

    Lagt til logikk for circuitBreaker og timeouthåndtering for kall til Inntektskomponeten

--- src/main/kotlin/no/nav/bidrag/grunnlag/consumer/inntektskomponenten/InntektskomponentenConsumer.kt
+++ src/main/kotlin/no/nav/bidrag/grunnlag/consumer/inntektskomponenten/InntektskomponentenConsumer.kt
@@ -1,6 +1,5 @@
 package no.nav.bidrag.grunnlag.consumer.inntektskomponenten
 
-import io.github.resilience4j.timelimiter.TimeLimiter
 import io.github.resilience4j.timelimiter.TimeLimiterRegistry
 import no.nav.bidrag.commons.web.client.AbstractRestClient
 import no.nav.bidrag.grunnlag.SECURE_LOGGER
@@ -22,6 +21,10 @@ import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
+import java.util.concurrent.CancellationException
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CompletionException
+import java.util.concurrent.TimeoutException
 import javax.naming.ServiceUnavailableException
 
 @Service
@@ -29,8 +32,8 @@ class InntektskomponentenConsumer(
     @Value("\${INNTEKTSKOMPONENTEN_URL}") inntektskomponentenUrl: URI,
     @Qualifier("azureService") private val restTemplate: RestTemplate,
     private val grunnlagConsumer: GrunnlagConsumer,
-    private val circuitBreakerFactory: CircuitBreakerFactory<*, *>,
-    private val timeLimiterRegistry: TimeLimiterRegistry,
+    circuitBreakerFactory: CircuitBreakerFactory<*, *>,
+    timeLimiterRegistry: TimeLimiterRegistry,
 ) : AbstractRestClient(restTemplate, "inntektskomponenten") {
 
     private val circuitBreaker = circuitBreakerFactory.create("inntektskomponenten")
@@ -51,74 +54,107 @@ class InntektskomponentenConsumer(
             .toUriString()
 
     fun hentInntekter(request: HentInntektListeRequest, abonnerteInntekterRequest: Boolean): RestResponse<HentInntektListeResponse> =
-        TimeLimiter.decorate(timeLimiter) {
-            circuitBreaker.run({
-                val url = if (abonnerteInntekterRequest) hentAbonnerteInntekterUri else hentInntekterUri
+        circuitBreaker.run({
+            timeLimiter.executeFutureSupplier {
+                CompletableFuture.supplyAsync {
+                    val url = if (abonnerteInntekterRequest) hentAbonnerteInntekterUri else hentInntekterUri
 
-                val restResponse = restTemplate.tryExchange(
-                    url = url,
-                    httpMethod = HttpMethod.POST,
-                    httpEntity = grunnlagConsumer.initHttpEntityInntektskomponenten(request),
-                    responseType = HentInntektListeResponse::class.java,
-                    fallbackBody = HentInntektListeResponse(emptyList(), Aktoer(request.ident.identifikator, AktoerType.NATURLIG_IDENT)),
-                )
-                when (restResponse) {
-                    is RestResponse.Success -> SECURE_LOGGER.info(
-                        "Henting av abonnerte ? $abonnerteInntekterRequest inntekter for perioden ${request.maanedFom} - ${request.maanedTom} " +
-                            "ga følgende respons for ${request.ident.identifikator}: ${tilJson(restResponse.body)}",
+                    val restResponse = restTemplate.tryExchange(
+                        url = url,
+                        httpMethod = HttpMethod.POST,
+                        httpEntity = grunnlagConsumer.initHttpEntityInntektskomponenten(request),
+                        responseType = HentInntektListeResponse::class.java,
+                        fallbackBody = HentInntektListeResponse(emptyList(), Aktoer(request.ident.identifikator, AktoerType.NATURLIG_IDENT)),
                     )
+                    when (restResponse) {
+                        is RestResponse.Success -> SECURE_LOGGER.info(
+                            "Henting av abonnerte ? $abonnerteInntekterRequest inntekter for perioden ${request.maanedFom} - ${request.maanedTom} " +
+                                "ga følgende respons for ${request.ident.identifikator}: ${tilJson(restResponse.body)}",
+                        )
 
-                    is RestResponse.Failure -> {
-                        if (abonnerteInntekterRequest) {
-                            // Utelater feillogging ved 400 - Bad Request (inntektsabonnement finnes ikke for personen)
-                            // og 423 og 500 (inntektsabonnementet er ikke aktivt ennå)
-                            if (restResponse.statusCode == HttpStatus.NOT_FOUND ||
-                                restResponse.statusCode == HttpStatus.INTERNAL_SERVER_ERROR ||
-                                restResponse.statusCode == HttpStatus.BAD_REQUEST ||
-                                restResponse.statusCode == HttpStatus.LOCKED
-                            ) {
-                                InntektskomponentenService.LOGGER.warn(
-                                    "Mangler abonnement for henting av inntekter fra Inntektskomponenten. " +
-                                        "Statuskode ${restResponse.statusCode.value()}/${restResponse.message}",
-                                )
-                                SECURE_LOGGER.warn(
-                                    "Mangler abonnement for henting av inntekter fra Inntektskomponenten for ${request.ident.identifikator} for " +
-                                        "perioden ${request.maanedFom} - ${request.maanedTom}." +
-                                        " Prøver å hente inntekter uten abonnement. ${restResponse.message}",
-                                )
+                        is RestResponse.Failure -> {
+                            if (abonnerteInntekterRequest) {
+                                // Utelater feillogging ved 400 - Bad Request (inntektsabonnement finnes ikke for personen)
+                                // og 423 og 500 (inntektsabonnementet er ikke aktivt ennå)
+                                if (restResponse.statusCode == HttpStatus.NOT_FOUND ||
+                                    restResponse.statusCode == HttpStatus.INTERNAL_SERVER_ERROR ||
+                                    restResponse.statusCode == HttpStatus.BAD_REQUEST ||
+                                    restResponse.statusCode == HttpStatus.LOCKED
+                                ) {
+                                    InntektskomponentenService.LOGGER.warn(
+                                        "Mangler abonnement for henting av inntekter fra Inntektskomponenten. " +
+                                            "Statuskode ${restResponse.statusCode.value()}/${restResponse.message}",
+                                    )
+                                    SECURE_LOGGER.warn(
+                                        "Mangler abonnement for henting av inntekter fra Inntektskomponenten for ${request.ident.identifikator} " +
+                                            "for perioden ${request.maanedFom} - ${request.maanedTom}." +
+                                            " Prøver å hente inntekter uten abonnement. ${restResponse.message}",
+                                    )
+                                } else {
+                                    InntektskomponentenService.LOGGER.error(
+                                        "Feil ved henting av inntekter med abonnement fra Inntektskomponenten. " +
+                                            "Statuskode ${restResponse.statusCode.value()}/${restResponse.message}",
+                                    )
+                                    SECURE_LOGGER.error(
+                                        "Feil ved henting av inntekter med abonnement for ${request.ident.identifikator} for perioden " +
+                                            "${request.maanedFom} - ${request.maanedTom}. Prøver å hente inntekter uten abonnement. /${restResponse.message}",
+                                    )
+                                }
                             } else {
                                 InntektskomponentenService.LOGGER.error(
-                                    "Feil ved henting av inntekter med abonnement fra Inntektskomponenten. " +
-                                        "Statuskode ${restResponse.statusCode.value()}/${restResponse.message}",
+                                    "Feil ved henting av inntekter uten abonnement fra Inntektskomponenten. " +
+                                        "Statuskode ${restResponse.statusCode.value()} /${restResponse.message}",
                                 )
                                 SECURE_LOGGER.error(
-                                    "Feil ved henting av inntekter med abonnement for ${request.ident.identifikator} for perioden " +
+                                    "Feil ved henting av inntekter uten abonnement for ${request.ident.identifikator} for perioden " +
                                         "${request.maanedFom} - ${request.maanedTom}. Prøver å hente inntekter uten abonnement. /${restResponse.message}",
                                 )
                             }
-                        } else {
-                            InntektskomponentenService.LOGGER.error(
-                                "Feil ved henting av inntekter uten abonnement fra Inntektskomponenten. " +
-                                    "Statuskode ${restResponse.statusCode.value()} /${restResponse.message}",
-                            )
-                            SECURE_LOGGER.error(
-                                "Feil ved henting av inntekter uten abonnement for ${request.ident.identifikator} for perioden " +
-                                    "${request.maanedFom} - ${request.maanedTom}. Prøver å hente inntekter uten abonnement. /${restResponse.message}",
-                            )
                         }
                     }
+
+                    grunnlagConsumer.logResponse(logger = SECURE_LOGGER, restResponse = restResponse)
+
+                    restResponse
                 }
+            }
+        }, { throwable ->
+            håndtereFeilMedKontekst(throwable, request, abonnerteInntekterRequest)
+        })
 
-                grunnlagConsumer.logResponse(logger = SECURE_LOGGER, restResponse = restResponse)
+    private fun håndtereFeilMedKontekst(
+        throwable: Throwable,
+        request: HentInntektListeRequest,
+        abonnerteInntekterRequest: Boolean,
+    ): RestResponse<HentInntektListeResponse> {
+        val ident = request.ident.identifikator
+        val periode = "${request.maanedFom} - ${request.maanedTom}"
+        val isTimeout = throwable is TimeoutException ||
+            throwable is CancellationException ||
+            (throwable is CompletionException && throwable.cause is TimeoutException)
 
-                restResponse
-            }, { throwable ->
-                håndtereFeil(throwable)
-            })
-        }.get()
+        if (Thread.currentThread().isInterrupted) {
+            Thread.interrupted()
+            SECURE_LOGGER.warn("Tråd ble avbrutt under kall til inntektskomponenten for: $ident")
+        }
 
-    private fun håndtereFeil(throwable: Throwable): RestResponse<HentInntektListeResponse> {
-        SECURE_LOGGER.error("Circuit breaker-logikk iverksatt for inntektskomponenten: ${throwable.message}")
-        return RestResponse.Failure("Inntektskomponenten svarer ikke", HttpStatus.SERVICE_UNAVAILABLE, ServiceUnavailableException())
+        if (isTimeout) {
+            InntektskomponentenService.LOGGER.error(
+                "Timeout ved kall til inntektskomponenten (${if (abonnerteInntekterRequest) "abonnerte" else "vanlige"} inntekter)",
+                throwable,
+            )
+            SECURE_LOGGER.error(
+                "Timeout ved henting av ${if (abonnerteInntekterRequest) "abonnerte" else "vanlige"} inntekter for $ident i perioden $periode",
+                throwable,
+            )
+        } else {
+            SECURE_LOGGER.error("Circuit breaker aktivert for inntektskomponenten: ${throwable.message}", throwable)
+        }
+
+        return RestResponse.Failure(
+            "Inntektskomponenten svarer ikke" + if (isTimeout) " (timeout)" else "",
+            HttpStatus.SERVICE_UNAVAILABLE,
+            ServiceUnavailableException(throwable.message),
+        )
     }
 }
